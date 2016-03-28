3b88aae7a5c24b7611dea4ca3355d8623524b724
jxcjean <jxcjean@users.noreply.github.com>
2016-03-28 21:11:02 +0800

    Added files via upload

---
+++ gui.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()  # 创建一个根窗口
+root.title("Stock trade system AgeanSea Project 2.0")   #Tk标题
+root.geometry('900x600')
+root.resizable(width=True,height=True)
+
+
+
+root.mainloop()  # 进入窗口的主循环，否则无法显示界面
