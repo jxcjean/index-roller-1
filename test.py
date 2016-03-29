5eed0c296073de83b4548ac3b44875fffe5a3c40
Alex Jiang <jxcjean@users.noreply.github.com>
2016-03-29 19:17:27 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-
-result_dict = {'510050': {'now': 2, 'date': 5, 'time': 8},
-               '159915': {'now': 3, 'date': 6, 'time': 9},
-               '150023': {'now': 4, 'date': 7, 'time': 10}}
-
-aa = list(result_dict.keys(),'test')
-bb = list(result_dict.values())
-print(aa)
-print(bb)
